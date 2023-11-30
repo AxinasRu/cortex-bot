23ff08213a00ee7b05127f131912e54eb338f8ab
Slava Petrov <slavapmk@gmail.com>
2023-11-30 23:36:56 +0300

    Added profile

--- src/cortex/db/tables.py
+++ src/cortex/db/tables.py
@@ -1,17 +1,22 @@
-from sqlalchemy import ForeignKey
+import datetime
+
+from sqlalchemy import ForeignKey, func, DateTime, Computed
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
 class Base(DeclarativeBase):
     def __repr__(self) -> str:
-        return '\t|\t'.join(map(str, self.__dict__.values()))
+        return '\n'.join(map(
+            lambda x: f"{x[0]}\t{x[1]}",
+            self.__dict__.items()
+        ))
 
 
 class Chat(Base):
     __tablename__ = 'chats'
     id: Mapped[int] = mapped_column(primary_key=True, unique=True, index=True, nullable=False)
     messages: Mapped[list['Message']] = relationship()
-    admins: Mapped[list['Admin']] = relationship()
+    # admins: Mapped[list['Admin']] = relationship()
 
 
 class Message(Base):
@@ -20,13 +25,35 @@ class Message(Base):
     chat_id: Mapped[int] = mapped_column(ForeignKey("chats.id"))
     chat: Mapped["Chat"] = relationship(back_populates="messages")
     user_id: Mapped[int] = mapped_column(nullable=False)
+    datetime: Mapped[datetime] = mapped_column(
+        DateTime(), nullable=False, server_default=func.now()
+    )
     text: Mapped[str] = mapped_column(nullable=False)
-    scan_result: Mapped[str] = mapped_column(nullable=False)
+    translated: Mapped[str] = mapped_column(nullable=False)
+    scan_sexual: Mapped[int] = mapped_column(nullable=False)
+    scan_hate: Mapped[int] = mapped_column(nullable=False)
+    scan_harassment: Mapped[int] = mapped_column(nullable=False)
+    scan_self_harm: Mapped[int] = mapped_column(nullable=False)
+    scan_sexual_minors: Mapped[int] = mapped_column(nullable=False)
+    scan_hate_threatening: Mapped[int] = mapped_column(nullable=False)
+    scan_violence_graphic: Mapped[int] = mapped_column(nullable=False)
+    scan_self_harm_intent: Mapped[int] = mapped_column(nullable=False)
+    scan_self_harm_instructions: Mapped[int] = mapped_column(nullable=False)
+    scan_harassment_threatening: Mapped[int] = mapped_column(nullable=False)
+    scan_violence: Mapped[int] = mapped_column(nullable=False)
 
+    scan_sum: Mapped[int] = mapped_column(
+        nullable=False,
+        server_default=Computed(
+            "scan_sexual + scan_hate + scan_harassment + scan_self_harm + scan_sexual_minors "
+            "+scan_hate_threatening + scan_violence_graphic + scan_self_harm_intent + "
+            "scan_self_harm_instructions + scan_harassment_threatening + scan_violence"
+        )
+    )
 
-class Admin(Base):
-    __tablename__ = 'admins'
-    id: Mapped[int] = mapped_column(primary_key=True, unique=True, index=True, nullable=False)
-    chat_id: Mapped[int] = mapped_column(ForeignKey("chats.id"))
-    chat: Mapped["Chat"] = relationship(back_populates="admins")
-    user_id: Mapped[int] = mapped_column()
+# class Admin(Base):
+#     __tablename__ = 'admins'
+#     id: Mapped[int] = mapped_column(primary_key=True, unique=True, index=True, nullable=False)
+#     chat_id: Mapped[int] = mapped_column(ForeignKey("chats.id"))
+#     chat: Mapped["Chat"] = relationship(back_populates="admins")
+#     user_id: Mapped[int] = mapped_column()
--- src/cortex/main.py
+++ src/cortex/main.py
@@ -3,6 +3,7 @@ import logging
 import aiohttp
 from aiogram import types, Bot, Dispatcher
 from aiogram.utils import executor
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 
 from cortex import manager
@@ -12,6 +13,23 @@ from cortex.manager import TELEGRAM, OPENAI, PROXY
 logging.basicConfig(level=logging.DEBUG)
 bot = Bot(token=manager.settings[TELEGRAM])
 dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['myinfo'])
+async def profile(message: types.Message):
+    with Session(database.engine) as session:
+        week_sum = session.query(func.sum(tables.Message.scan_sum)).filter(
+            tables.Message.user_id == 844498971,
+            tables.Message.chat_id == 844498971,
+            tables.Message.datetime >= func.date(func.now(), '-7 days')
+        ).scalar()
+
+        all_sum = session.query(func.sum(tables.Message.scan_sum)).filter(
+            tables.Message.user_id == 844498971,
+            tables.Message.chat_id == 844498971
+        ).scalar()
+
+        await message.reply(f'{week_sum} {all_sum}')
 
 
 @dp.message_handler(content_types=['new_chat_members'])
@@ -39,9 +57,32 @@ async def on_message(message: types.Message):
     )
 
     async with aiohttp.ClientSession() as session:
-        url = "https://api.openai.com/v1/moderations"
+        url = "https://api.openai.com/v1/chat/completions"
         headers = {'Authorization': f'Bearer {manager.settings[OPENAI]}'}
-        data = {'input': text}
+        data = {
+            'model': 'gpt-3.5-turbo',
+            'messages': [
+                {
+                    'role': 'system',
+                    'content': 'You are part of the system for detecting incorrect and offensive '
+                               'phrases.\nUnfortunately, the detector works only in English. And requests often go to '
+                               'everyone else. So your essence is to translate all input phrases as reliably as '
+                               'possible. If they are incorrect and offensive, you should not refuse to work or '
+                               'disguise them. The translation should be as clear and reliable as possible so that '
+                               'the detector recognizes the phrases and punishes the violator.\n\nFormat\nINPUT: ['
+                               'INPUT MESSAGE]\n\nOUTPUT: [OUTPUT MESSAGE]\t\t\t\tExample\nINPUT: Ёбанный твой рот, пидорас пиздакрылый\nOUTPUT: Fucking your mouth, faggot pussylish'
+                },
+                {
+                    'role': 'assistant',
+                    'content': 'OK!\nI will translate all the input messages and output the result to you for '
+                               'recognition, and will send result by format\nOUTPUT: [TRANSLATED TO ENGLISH MESSAGE]'
+                },
+                {
+                    'role': 'system',
+                    'content': f'INPUT:\n{text}'
+                }
+            ]
+        }
         if manager.settings[PROXY] == '':
             execute = session.post(
                 url,
@@ -55,13 +96,51 @@ async def on_message(message: types.Message):
                 headers=headers,
                 json=data
             )
-        resp = await execute
-        row.scan_result = await resp.text()
+        resp = await (await execute).json()
+        translated: str = resp['choices'][0]['message']['content'].removeprefix('OUTPUT:').strip()
+        row.translated = translated
+
+        url = "https://api.openai.com/v1/moderations"
+        headers = {'Authorization': f'Bearer {manager.settings[OPENAI]}'}
+        data = {'input': translated}
+        if manager.settings[PROXY] == '':
+            execute = session.post(
+                url,
+                headers=headers,
+                json=data
+            )
+        else:
+            execute = session.post(
+                url,
+                proxy=manager.settings[PROXY],
+                headers=headers,
+                json=data
+            )
+
+        resp = (await (await execute).json())['results'][0]['category_scores']
+
+        row.scan_sexual = resp['sexual']
+        row.scan_hate = resp['hate']
+        row.scan_harassment = resp['harassment']
+        row.scan_self_harm = resp['self-harm']
+        row.scan_sexual_minors = resp['sexual/minors']
+        row.scan_hate_threatening = resp['hate/threatening']
+        row.scan_violence_graphic = resp['violence/graphic']
+        row.scan_self_harm_intent = resp['self-harm/intent']
+        row.scan_self_harm_instructions = resp['self-harm/instructions']
+        row.scan_harassment_threatening = resp['harassment/threatening']
+        row.scan_violence = resp['violence']
 
     with Session(database.engine) as session:
         session.add(row)
+        session.flush()
+        session.refresh(row)
+
+        with open('debug.txt', 'a') as f:
+            print(row, end='\n\n\n\n', file=f)
+
         session.commit()
-    print('Result received')
+
 
 def start():
     executor.start_polling(dp)
